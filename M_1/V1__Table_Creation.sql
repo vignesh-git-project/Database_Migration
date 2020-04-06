37e81cce8f4d74b19987c40839db82c0067f69cc
vignesh-git-project <56680289+vignesh-git-project@users.noreply.github.com>
2020-04-06 19:13:02 +0530

    Add files via upload

---
+++ M_1/V1__Table_Creation.sql
@@ -0,0 +1,15 @@
+
+CREATE TABLE Manufacturers (
+  Code INTEGER,
+  Name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (Code)   
+);
+
+CREATE TABLE Products (
+  Code INTEGER,
+  Name VARCHAR(255) NOT NULL ,
+  Price DECIMAL NOT NULL ,
+  Manufacturer INTEGER NOT NULL,
+  PRIMARY KEY (Code), 
+  FOREIGN KEY (Manufacturer) REFERENCES Manufacturers(Code)
+);
